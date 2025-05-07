26a4881ef68426eba7bcdef8cf82c0deee63932f
Krithika Govind (from Dev Box) <krigovind@microsoft.com>
2025-05-07 11:41:22 -0700

    hider UI with category uploads working

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(express.static('public'));
 const admin = require('firebase-admin');
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
+// Fix the private_key formatting (replace literal `\n` with actual newlines)
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://scavenger-hunt-4173c-default-rtdb.firebaseio.com"
